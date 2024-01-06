826164c9f82c419f0ee4e120069e1bce9865cbf2
Renan Couto <renanmv10@hotmail.com>
2024-01-06 18:02:55 -0300

    Implementando a verificacao de xeque

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 using System.Runtime.ConstrainedExecution;
 
-namespace xadrez_console.tabuleiro
-{
-    abstract class Peca
-    {
+namespace xadrez_console.tabuleiro {
+    abstract class Peca {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Tabuleiro tab, Cor cor)
-        {
+        public Peca(Tabuleiro tab, Cor cor) {
             this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
         }
 
-        public void incrementarQteMovimentos()
-        {
+        public void incrementarQteMovimentos() {
             qteMovimentos++;
+        }
+
+        public void decrementarQteMovimentos() {
+            qteMovimentos--;
         }
 
         public bool existeMovimentosPossiveis() {
             bool[,] mat = movimentosPossiveis();
-            for(int i = 0; i < tab.linhas;  i++) {
-                for(int j = 0; j < tab.colunas; j++) {
+            for (int i = 0; i < tab.linhas; i++) {
+                for (int j = 0; j < tab.colunas; j++) {
                     if (mat[i, j]) {
                         return true;
                     }
