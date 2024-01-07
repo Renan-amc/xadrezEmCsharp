3f59d96d7d5af350522a2e7b30565220a8222a3e
Renan Couto <renanmv10@hotmail.com>
2024-01-07 15:49:36 -0300

    jogada especial roque pequeno e roque grande

--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -5,8 +5,11 @@ namespace xadrez_console.xadrez
 {
     internal class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        private PartidaDeXadrez partida;
+
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -18,6 +21,11 @@ namespace xadrez_console.xadrez
         {
             Peca p = tab.peca(pos);
             return p == null || p.cor != cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos) {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qteMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis()
@@ -80,6 +88,33 @@ namespace xadrez_console.xadrez
             if (tab.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
+            }
+
+            // #jogadaEspecial Roque
+            if(qteMovimentos == 0 && !partida.xeque) {
+                // #jogadaEspecail Roque Pequeno
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                //identifico a posicao da torre e armazeno
+                if (testeTorreParaRoque(posT1)) {
+                //verifico se a posicao na qual peguei é uma torre não é nula, é da mesma cor...
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if(tab.peca(p1) == null && tab.peca(p2) == null) {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+                // #jogadaEspecail Roque Grande
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                //identifico a posicao da torre e armazeno
+                if (testeTorreParaRoque(posT2)) {
+                    //verifico se a posicao na qual peguei é uma torre não é nula, é da mesma cor...
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null) {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
             }
 
             return mat;
